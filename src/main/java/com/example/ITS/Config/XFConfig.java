74ca48621156564b3ad71e4f1cc3fcf0d8b6bcff
Azoic <1515242043@qq.com>
2024-06-17 18:25:35 +0800

    Spark Fixed

--- src/main/java/com/example/ITS/Config/XFConfig.java
+++ src/main/java/com/example/ITS/Config/XFConfig.java
@@ -1,5 +1,6 @@
 package com.example.ITS.Config;
 
+import io.github.briqt.spark4j.SparkClient;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -8,15 +9,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "spark.config")
 @Data
-public class SparkClient {
-    private String appId;
+public class XFConfig {
+    private String appid;
     private String apiSecret;
     private String apiKey;
 
     @Bean
     public SparkClient sparkConfig(){
         SparkClient sparkClient = new SparkClient();
-        sparkClient.appId = appId;
+        sparkClient.appid = appid;
         sparkClient.apiKey = apiKey;
         sparkClient.apiSecret = apiSecret;
         return sparkClient;
